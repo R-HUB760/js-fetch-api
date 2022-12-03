c104226f33ea93fd496ccb154dbe898b67f8aff6
Abdur Rahim <60249128+R-HUB760@users.noreply.github.com>
2022-12-03 18:43:29 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function fetchApi(){
+  let ftc = fetch("https://api.kanye.rest");
+   try {
+    ftc.then((res) => res.json())
+    .then((data) => showDataFromApi(data));
+   } catch (error) {
+    console.log(error);
+   }
+}
+
+
+function showDataFromApi(data){
+  document.querySelector('.blockquote').innerHTML = data.quote;
+}
+
+setInterval(fetchApi, 4000);
